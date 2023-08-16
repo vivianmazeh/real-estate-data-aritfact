53a1da0f26d38c21940510bab3e0e56f0aa82e2c
vivianmazeh <vivian@mazeh.io>
2023-08-16 11:01:57 -0400

    init commit

---
+++ src/test/java/real_estate_data/AppTest.java
@@ -0,0 +1,20 @@
+package real_estate_data;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
